b9e8def0bf00539836dae871b3bbb22c9b106f5e
Martin Yeo <martin.yeo@metoffice.gov.uk>
2021-06-24 09:43:21 +0100

    conf.py stop importing cf_units.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -4,8 +4,6 @@
 # See COPYING and COPYING.LESSER in the root of the repository for full
 # licensing details.
 
-import cf_units
-
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.intersphinx",
